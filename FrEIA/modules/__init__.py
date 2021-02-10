627e40406077188bcbb89a8063f88c9a9c43c820
Lynton Ardizzone <lynton.ardizzone@iwr.uni-heidelberg.de>
2021-02-10 13:23:06 +0100

    AllInOneBlock: follows new interface, docstrings & comments, minor code improvements

--- FrEIA/modules/__init__.py
+++ FrEIA/modules/__init__.py
@@ -50,6 +50,7 @@ Reshaping:
 
 '''
 
+from .base import InvertibleModule
 from .all_in_one_block import *
 from .fixed_transforms import *
 from .reshapes import *
@@ -62,6 +63,7 @@ from .invertible_resnet import *
 from .gaussian_mixture import *
 
 __all__ = [
+            'InvertibleModule',
             'AllInOneBlock',
             'glow_coupling_layer',
             'rev_layer',
--- FrEIA/modules/all_in_one_block.py
+++ FrEIA/modules/all_in_one_block.py
@@ -1,5 +1,7 @@
-import pdb
+from . import InvertibleModule
+
 import warnings
+from typing import Callable
 
 import numpy as np
 import torch
@@ -7,99 +9,109 @@ import torch.nn as nn
 import torch.nn.functional as F
 from scipy.stats import special_ortho_group
 
-class AllInOneBlock(nn.Module):
-    ''' Combines affine coupling, permutation, global affine transformation ('ActNorm')
-    in one block.'''
+class AllInOneBlock(InvertibleModule):
+    '''Module combining the most common operations in a normalizing flow or similar model.
+
+    It combines affine coupling, permutation, and global affine transformation
+    ('ActNorm'). It can also be used as GIN coupling block, perform learned
+    householder permutations, and use an inverted pre-permutation (see
+    constructor docstring for details).'''
 
     def __init__(self, dims_in, dims_c=[],
-                 subnet_constructor=None,
-                 affine_clamping=2.,
-                 gin_block=False,
-                 global_affine_init=1.,
-                 global_affine_type='SOFTPLUS',
-                 permute_soft=False,
-                 learned_householder_permutation=0,
-                 reverse_permutation=False):
+                 subnet_constructor: Callable = None,
+                 affine_clamping: float = 2.,
+                 gin_block: bool = False,
+                 global_affine_init: float = 1.,
+                 global_affine_type: str = 'SOFTPLUS',
+                 permute_soft: bool = False,
+                 learned_householder_permutation: int = 0,
+                 reverse_permutation: bool = False):
         '''
-        subnet_constructor: class or callable f, called as
-            f(channels_in, channels_out) and should return a torch.nn.Module
-
-        affine_clamping: clamp the output of the mutliplicative coefficients
-            (before exponentiation) to +/- affine_clamping.
-
-        gin_block: Turn the block into a GIN block from Sorrenson et al, 2019
-
-        global_affine_init: Initial value for the global affine scaling beta
-
-        global_affine_init: 'SIGMOID', 'SOFTPLUS', or 'EXP'. Defines the activation
-            to be used on the beta for the global affine scaling.
-
-        permute_soft: bool, whether to sample the permutation matrices from SO(N),
-            or to use hard permutations in stead. Note, permute_soft=True is very slow
+        Args:
+          subnet_constructor:
+            class or callable f, called as f(channels_in, channels_out) and
+            should return a torch.nn.Module
+          affine_clamping:
+            clamp the output of the mutliplicative coefficients (before
+            exponentiation) to +/- affine_clamping.
+          gin_block:
+            Turn the block into a GIN block from Sorrenson et al, 2019
+          global_affine_init:
+            Initial value for the global affine scaling beta
+          global_affine_init:
+            'SIGMOID', 'SOFTPLUS', or 'EXP'. Defines the activation to be used
+            on the beta for the global affine scaling.
+          permute_soft:
+            bool, whether to sample the permutation matrices from SO(N), or to
+            use hard permutations in stead. Note, permute_soft=True is very slow
             when working with >512 dimensions.
-
-        learned_householder_permutation: Int, if >0,  use that many learned householder
-            reflections. Slow if large number. Dubious whether it actually helps.
-
-        reverse_permutation: Reverse the permutation before the block, as introduced by
-            Putzky et al, 2019.
+          learned_householder_permutation:
+            Int, if >0,  use that many learned householder reflections. Slow if
+            large number. Dubious whether it actually helps.
+          reverse_permutation:
+            Reverse the permutation before the block, as introduced by Putzky
+            et al, 2019.
         '''
 
-        super().__init__()
+        super().__init__(dims_in, dims_c)
 
         channels = dims_in[0][0]
-        self.Ddim = len(dims_in[0]) - 1
-        self.sum_dims = tuple(range(1, 2 + self.Ddim))
+        # rank of the tensors means 1d, 2d, 3d tensor etc.
+        self.input_rank = len(dims_in[0]) - 1
+        # tuple contiaining all dims except for batch-dim (used at various points)
+        self.sum_dims = tuple(range(1, 2 + self.input_rank))
 
         if len(dims_c) == 0:
             self.conditional = False
             self.condition_channels = 0
-        elif len(dims_c) == 1:
-            self.conditional = True
-            self.condition_channels = dims_c[0][0]
+        else:
             assert tuple(dims_c[0][1:]) == tuple(dims_in[0][1:]), \
                 F"Dimensions of input and condition don't agree: {dims_c} vs {dims_in}."
-        else:
-            raise ValueError('Only supports one condition (concatenate externally)')
+            self.conditional = True
+            self.condition_channels = sum(dc[0] for dc in dims_c)
 
         split_len1 = channels - channels // 2
         split_len2 = channels // 2
         self.splits = [split_len1, split_len2]
 
-
         try:
             self.permute_function = {0 : F.linear,
                                      1 : F.conv1d,
                                      2 : F.conv2d,
-                                     3 : F.conv3d}[self.Ddim]
+                                     3 : F.conv3d}[self.input_rank]
         except KeyError:
-            raise ValueError(f"Data has {1 + self.Ddim} dimensions. Must be 1-4.")
+            raise ValueError(f"Data is {1 + self.input_rank}D. Must be 1D-4D.")
 
         self.in_channels = channels
         self.clamp = affine_clamping
         self.GIN = gin_block
-        self.welling_perm = reverse_permutation
+        self.reverse_pre_permute = reverse_permutation
         self.householder = learned_householder_permutation
 
         if permute_soft and channels > 512:
             warnings.warn(("Soft permutation will take a very long time to initialize "
                            f"with {channels} feature channels. Consider using hard permutation instead."))
 
+        # global_scale is used as the initial value for the global affine scale
+        # (pre-activation). It is computed such that
+        # global_scale_activation(global_scale) = global_affine_init
+        # the 'magic numbers' (specifically for sigmoid) scale the activation to
+        # a sensible range.
         if global_affine_type == 'SIGMOID':
-            global_scale = np.log(global_affine_init)
+            global_scale = 2. - np.log(10. / global_affine_init -1.)
             self.global_scale_activation = (lambda a: 10 * torch.sigmoid(a - 2.))
         elif global_affine_type == 'SOFTPLUS':
-            global_scale = 10. * global_affine_init
+            global_scale = 2. * np.log(np.exp(0.5 * 10. * global_affine_init) - 1)
             self.softplus = nn.Softplus(beta=0.5)
             self.global_scale_activation = (lambda a: 0.1 * self.softplus(a))
         elif global_affine_type == 'EXP':
             global_scale = np.log(global_affine_init)
             self.global_scale_activation = (lambda a: torch.exp(a))
         else:
-            raise ValueError('Please, SIGMOID, SOFTPLUS or EXP, as global affine type')
+            raise ValueError('Global affine activation must be "SIGMOID", "SOFTPLUS" or "EXP"')
 
-        self.global_scale = nn.Parameter(torch.ones(1, self.in_channels, *([1] * self.Ddim)) * float(global_scale))
-        self.global_offset = nn.Parameter(torch.zeros(1, self.in_channels,  *([1] * self.Ddim)))
+        self.global_scale = nn.Parameter(torch.ones(1, self.in_channels, *([1] * self.input_rank)) * float(global_scale))
+        self.global_offset = nn.Parameter(torch.zeros(1, self.in_channels,  *([1] * self.input_rank)))
 
         if permute_soft:
             w = special_ortho_group.rvs(channels)
@@ -109,70 +121,94 @@ class AllInOneBlock(nn.Module):
                 w[i,j] = 1.
 
         if self.householder:
+            # instead of just the permutation matrix w, the learned housholder
+            # permutation keeps track of reflection vectors vk, in addition to a
+            # random initial permutation w_0.
             self.vk_householder = nn.Parameter(0.2 * torch.randn(self.householder, channels), requires_grad=True)
-            self.w = None
-            self.w_inv = None
+            self.w_perm = None
+            self.w_perm_inv = None
             self.w_0 = nn.Parameter(torch.FloatTensor(w), requires_grad=False)
         else:
-            self.w = nn.Parameter(torch.FloatTensor(w).view(channels, channels, *([1] * self.Ddim)),
+            self.w_perm = nn.Parameter(torch.FloatTensor(w).view(channels, channels, *([1] * self.input_rank)),
                                   requires_grad=False)
-            self.w_inv = nn.Parameter(torch.FloatTensor(w.T).view(channels, channels, *([1] * self.Ddim)),
+            self.w_perm_inv = nn.Parameter(torch.FloatTensor(w.T).view(channels, channels, *([1] * self.input_rank)),
                                   requires_grad=False)
 
-        self.s = subnet_constructor(self.splits[0] + self.condition_channels, 2 * self.splits[1])
+        if subnet_constructor is None:
+            raise ValueError("Please supply a callable subnet_constructor"
+                             "function or object (see docstring)")
+        self.subnet = subnet_constructor(self.splits[0] + self.condition_channels, 2 * self.splits[1])
         self.last_jac = None
 
-    def construct_householder_permutation(self):
+    def _construct_householder_permutation(self):
+        '''Computes a permutation matrix from the reflection vectors that are
+        learned internally as nn.Parameters.'''
         w = self.w_0
         for vk in self.vk_householder:
             w = torch.mm(w, torch.eye(self.in_channels).to(w.device) - 2 * torch.ger(vk, vk) / torch.dot(vk, vk))
 
-        for i in range(self.Ddim):
+        for i in range(self.input_rank):
             w = w.unsqueeze(-1)
         return w
 
-    def log_e(self, s):
-        s = self.clamp * torch.tanh(0.1 * s)
-        if self.GIN:
-            s -= torch.mean(s, dim=self.sum_dims, keepdim=True)
-        return s
-
-    def permute(self, x, rev=False):
+    def _permute(self, x, rev=False):
+        '''Performs the permutation and scaling after the coupling operation.
+        Returns transformed outputs and the LogJacDet of the scaling operation.'''
         if self.GIN:
             scale = 1.
+            perm_log_jac = 0.
         else:
-            scale = self.global_scale_activation( self.global_scale)
-        if rev:
-            return (self.permute_function(x, self.w_inv) - self.global_offset) / scale
-        else:
-            return self.permute_function(x * scale + self.global_offset, self.w)
+            scale = self.global_scale_activation(self.global_scale)
+            perm_log_jac = torch.sum(torch.log(scale))
 
-    def pre_permute(self, x, rev=False):
         if rev:
-            return self.permute_function(x, self.w)
+            return ((self.permute_function(x, self.w_perm_inv) - self.global_offset) / scale,
+                    perm_log_jac)
         else:
-            return self.permute_function(x, self.w_inv)
+            return (self.permute_function(x * scale + self.global_offset, self.w_perm),
+                   perm_log_jac)
 
-    def affine(self, x, a, rev=False):
+    def _pre_permute(self, x, rev=False):
+        '''Permutes before the coupling block, only used if
+        reverse_permutation is set'''
+        if rev:
+            return self.permute_function(x, self.w_perm)
+        else:
+            return self.permute_function(x, self.w_perm_inv)
+
+    def _affine(self, x, a, rev=False):
+        '''Given the passive half, and the pre-activation outputs of the
+        coupling subnetwork, perform the affine coupling operation.
+        Returns both the transformed inputs and the LogJacDet.'''
+
+        # the entire coupling coefficient tensor is scaled down by a
+        # factor of ten for stability and easier initialization.
+        a *= 0.1
         ch = x.shape[1]
-        sub_jac = self.log_e(a[:,:ch])
+
+        sub_jac = self.clamp * torch.tanh(a[:,:ch])
+        if self.GIN:
+            sub_jac -= torch.mean(sub_jac, dim=self.sum_dims, keepdim=True)
+
         if not rev:
-            return (x * torch.exp(sub_jac) + 0.1 * a[:,ch:],
+            return (x * torch.exp(sub_jac) + a[:,ch:],
                     torch.sum(sub_jac, dim=self.sum_dims))
         else:
-            return ((x - 0.1 * a[:,ch:]) * torch.exp(-sub_jac),
+            return ((x - a[:,ch:]) * torch.exp(-sub_jac),
                     -torch.sum(sub_jac, dim=self.sum_dims))
 
-    def forward(self, x, c=[], rev=False):
+    def forward(self, x, c=[], rev=False, jac=True):
+        '''See base class docstring'''
         if self.householder:
-            self.w = self.construct_householder_permutation()
-            if rev or self.welling_perm:
-                self.w_inv = self.w.transpose(0,1).contiguous()
+            self.w_perm = self._construct_householder_permutation()
+            if rev or self.reverse_pre_permute:
+                self.w_perm_inv = self.w_perm.transpose(0,1).contiguous()
 
         if rev:
-            x = [self.permute(x[0], rev=True)]
-        elif self.welling_perm:
-            x = [self.pre_permute(x[0], rev=False)]
+            x, global_scaling_jac = self._permute(x[0], rev=True)
+            x = (x,)
+        elif self.reverse_pre_permute:
+            x = (self._pre_permute(x[0], rev=False),)
 
         x1, x2 = torch.split(x[0], self.splits, dim=1)
 
@@ -182,31 +218,27 @@ class AllInOneBlock(nn.Module):
             x1c = x1
 
         if not rev:
-            a1 = self.s(x1c)
-            x2, j2 = self.affine(x2, a1)
+            a1 = self.subnet(x1c)
+            x2, j2 = self._affine(x2, a1)
         else:
-            # names of x and y are swapped!
-            a1 = self.s(x1c)
-            x2, j2 = self.affine(x2, a1, rev=True)
+            a1 = self.subnet(x1c)
+            x2, j2 = self._affine(x2, a1, rev=True)
 
-        self.last_jac = j2
+        log_jac_det = j2
         x_out = torch.cat((x1, x2), 1)
 
-        n_pixels = 1
-        for d in self.sum_dims[1:]:
-            n_pixels *= x_out.shape[d]
-
-        self.last_jac += ((-1)**rev * n_pixels) * (1 - int(self.GIN)) * (torch.log(self.global_scale_activation(self.global_scale) + 1e-12).sum())
-
         if not rev:
-            x_out = self.permute(x_out, rev=False)
-        elif self.welling_perm:
-            x_out = self.pre_permute(x_out, rev=True)
+            x_out, global_scaling_jac = self._permute(x_out, rev=False)
+        elif self.reverse_pre_permute:
+            x_out = self._pre_permute(x_out, rev=True)
 
-        return [x_out]
+        # add the global scaling jacobian to the total.
+        # trick to get the total number of non-channel dimensions:
+        # number of elements of the first channel of the first batch member
+        n_pixels = x_out[0, :1].numel()
+        log_jac_det += (-1)**rev * n_pixels * global_scaling_jac
 
-    def jacobian(self, x, c=[], rev=False):
-        return self.last_jac
+        return (x_out,), log_jac_det
 
     def output_dims(self, input_dims):
         return input_dims
