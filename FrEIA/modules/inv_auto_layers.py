12c996559b86cee3fbdfcb8b67b8cfdd9b57e43b
Lynton Ardizzone <lynton.ardizzone@iwr.uni-heidelberg.de>
2021-02-12 14:01:46 +0100

    new calls and return signatures for the invertible autoencoder layers

--- FrEIA/modules/inv_auto_layers.py
+++ FrEIA/modules/inv_auto_layers.py
@@ -1,16 +1,17 @@
+from . import InvertibleModule
+
 from copy import deepcopy
 
 import numpy as np
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as f
 
 
-class InvAutoActTwoSided(nn.Module):
+class InvAutoActTwoSided(InvertibleModule):
 
-    def __init__(self, dims_in, clamp=5.):
-        super().__init__()
+    def __init__(self, dims_in, dims_c=None, clamp=5.):
+        super().__init__(dims_in, dims_c)
         self.clamp = clamp
         self.alpha_pos = nn.Parameter(0.05 * torch.randn(dims_in[0][0]) + 0.7)
         self.alpha_neg = nn.Parameter(0.05 * torch.randn(dims_in[0][0]) - 0.7)
@@ -23,29 +24,29 @@ class InvAutoActTwoSided(nn.Module):
         return self.clamp * 0.636 * torch.atan(s/self.clamp)
 
     def forward(self, x, rev=False):
+       j = (-1)**rev * torch.sum(self.log_e(self.alpha_pos + 0.5 * (self.alpha_neg - self.alpha_pos) * (1 - x[0].sign())), dim=1)
         if not rev:
-            return [x[0] * self.e(self.alpha_pos + 0.5 * (self.alpha_neg - self.alpha_pos) * (1 - x[0].sign()))]
+            return [x[0] * self.e(self.alpha_pos + 0.5 * (self.alpha_neg - self.alpha_pos) * (1 - x[0].sign()))], j
         else:
-            return [x[0] * self.e(-self.alpha_pos - 0.5 * (self.alpha_neg - self.alpha_pos) * (1 - x[0].sign()))]
+            return [x[0] * self.e(-self.alpha_pos - 0.5 * (self.alpha_neg - self.alpha_pos) * (1 - x[0].sign()))], j
 
     def output_dims(self, input_dims):
         assert len(input_dims) == 1, "Can only use 1 input"
         return input_dims
 
-    def jacobian(self, x, rev=False):
-       return (-1)**rev * torch.sum(self.log_e(self.alpha_pos + 0.5 * (self.alpha_neg - self.alpha_pos) * (1 - x[0].sign())), dim=1)
 
-class InvAutoAct(nn.Module):
+class InvAutoAct(InvertibleModule):
 
-    def __init__(self, dims_in):
-        super().__init__()
+    def __init__(self, dims_in, dims_c=None):
+        super().__init__(dims_in, dims_c)
         self.alpha = nn.Parameter(0.01 * torch.randn(dims_in[0][0]) + 0.7)
 
     def forward(self, x, rev=False):
+        # TODO this one didn't even HAVE a jacobian implemented ?!?!
         if not rev:
-            return [x[0] * torch.exp(self.alpha * x[0].sign())]
+            return [x[0] * torch.exp(self.alpha * x[0].sign())], None
         else:
-            return [x[0] * torch.exp(self.alpha * x[0].sign().neg_())]
+            return [x[0] * torch.exp(self.alpha * x[0].sign().neg_())], None
 
     def output_dims(self, input_dims):
         assert len(input_dims) == 1, "Can only use 1 input"
@@ -54,48 +55,46 @@ class InvAutoAct(nn.Module):
 
 class InvAutoActFixed(nn.Module):
 
-    def __init__(self, dims_in, alpha=2.0):
-        super().__init__()
+    def __init__(self, dims_in, dims_c=None, alpha=2.0):
+        super().__init__(dims_in, dims_c)
         self.alpha = alpha
         self.alpha_inv = 1. / alpha
 
         self.log_alpha = np.log(alpha)
 
     def forward(self, x, rev=False):
+        j = (-1)**rev * torch.sum(self.log_alpha * x[0].sign(), dim=1)
         if not rev:
-            return [self.alpha_inv * f.leaky_relu(x[0], self.alpha*self.alpha)]
+            return [self.alpha_inv * f.leaky_relu(x[0], self.alpha*self.alpha)], j
         else:
-            return [self.alpha * f.leaky_relu(x[0], self.alpha_inv*self.alpha_inv)]
+            return [self.alpha * f.leaky_relu(x[0], self.alpha_inv*self.alpha_inv)], j
 
     def output_dims(self, input_dims):
         assert len(input_dims) == 1, "Can only use 1 input"
         return input_dims
 
-    def jacobian(self, x, rev=False):
-        return (-1)**rev * torch.sum(self.log_alpha * x[0].sign(), dim=1)
 
+class LearnedElementwiseScaling(InvertibleModule):
 
-class LearnedElementwiseScaling(nn.Module):
-
-    def __init__(self, dims_in):
-        super().__init__()
+    def __init__(self, dims_in, dims_c=None):
+        super().__init__(dims_in, dims_c)
         self.s = nn.Parameter(torch.zeros(*dims_in[0]))
 
     def forward(self, x, rev=False):
         if not rev:
-            return [x[0] * self.s.exp()]
+            return [x[0] * self.s.exp()], None
         else:
-            return [x[0] * self.s.neg().exp_()]
+            return [x[0] * self.s.neg().exp_()], None
 
     def output_dims(self, input_dims):
         assert len(input_dims) == 1, "Can only use 1 input"
         return input_dims
 
 
-class InvAutoFC(nn.Module):
+class InvAutoFC(InvertibleModule):
 
-    def __init__(self, dims_in, dims_out=None):
-        super().__init__()
+    def __init__(self, dims_in, dims_c, dims_out=None):
+        super().__init__(dims_in, dims_c)
         self.dims_in = dims_in
         if dims_out is None:
             self.dims_out = deepcopy(dims_in)
@@ -107,19 +106,19 @@ class InvAutoFC(nn.Module):
 
     def forward(self, x, rev=False):
         if not rev:
-            return [f.linear(x[0], self.weights) + self.bias.expand(x[0].size()[0], *self.dims_out[0])]
+            return [f.linear(x[0], self.weights) + self.bias.expand(x[0].size()[0], *self.dims_out[0])], None
         else:
-            return [f.linear(x[0] - self.bias.expand(x[0].size()[0], *self.dims_out[0]), self.weights.t())]
+            return [f.linear(x[0] - self.bias.expand(x[0].size()[0], *self.dims_out[0]), self.weights.t())], None
 
     def output_dims(self, input_dims):
         assert len(input_dims) == 1, "Can only use 1 input"
         return self.dims_out
 
 
-class InvAutoConv2D(nn.Module):
+class InvAutoConv2D(InvertibleModule):
 
-    def __init__(self, dims_in, dims_out, kernel_size=3, padding=1):
-        super().__init__()
+    def __init__(self, dims_in, dims_c=None, dims_out, kernel_size=3, padding=1):
+        super().__init__(dims_in, dims_c)
         self.dims_in = dims_in
         self.dims_out = dims_out
         self.kernel_size = kernel_size
@@ -136,7 +135,7 @@ class InvAutoConv2D(nn.Module):
             out = x[0] - self.bias.expand(x[0].size())
             out = f.conv_transpose2d(out, self.conv2d.weight, bias=None, padding=self.padding)
 
-        return [out]
+        return [out], None
 
     def output_dims(self, input_dims):
         assert len(input_dims) == 1, "Can only use 1 input"
