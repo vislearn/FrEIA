a0e377bc9135fd834b7b9a26d427ed29be9254ce
Lynton Ardizzone <lynton.ardizzone@iwr.uni-heidelberg.de>
2021-02-24 12:41:50 +0100

    invertible autoencoder layers & fixed crash with fixed permutation

--- FrEIA/modules/fixed_transforms.py
+++ FrEIA/modules/fixed_transforms.py
@@ -31,11 +31,8 @@ class PermuteRandom(InvertibleModule):
         for i, p in enumerate(self.perm):
             self.perm_inv[p] = i
 
-        self.perm = nn.Parameter(torch.LongTensor(self.perm))
-        self.perm.requires_grad = False
-
-        self.perm_inv = nn.Parameter(torch.LongTensor(self.perm_inv))
-        self.perm_inv.requires_grad = False
+        self.perm = nn.Parameter(torch.LongTensor(self.perm), requires_grad=False)
+        self.perm_inv = nn.Parameter(torch.LongTensor(self.perm_inv), requires_grad=False)
 
     def forward(self, x, rev=False, jac=True):
         if not rev:
