4bf96bc96fd7de0b3d1e113424c3a8c6e416e2d7
Felix Draxle <felix.draxler@iwr.uni-heidelberg.de>
2023-02-03 12:20:32 +0100

    Replace individual layer in SequenceINN

--- tests/test_sequence.py
+++ tests/test_sequence.py
@@ -25,3 +25,21 @@ class SequenceINNTest(unittest.TestCase):
         inn = SequenceINN(*input_shape)
         inn.append(AllInOneBlock(inn.output_dims(), subnet_constructor=subnet))
         self.assertEqual(inn.shapes[-1], input_shape)
+
+    def test_replace_layer(self):
+        dim = 2
+        inn = SequenceINN(dim)
+        for _ in range(3):
+            inn.append(AllInOneBlock(inn.output_dims(), subnet_constructor=subnet))
+
+        new_block = AllInOneBlock([inn.shapes[1]], subnet_constructor=subnet)
+        inn[1] = new_block
+        self.assertTrue(inn[1] is new_block)
+
+        # Wrong dimension
+        with self.assertRaises(ValueError):
+            inn[1] = AllInOneBlock([(dim + 1,)], subnet_constructor=subnet)
+
+        # Block suddenly got condition
+        with self.assertRaises(ValueError):
+            inn[1] = AllInOneBlock([(dim,)], dims_c=[(dim,)], subnet_constructor=subnet)
