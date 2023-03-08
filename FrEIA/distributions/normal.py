7f63827f3a7c33ec6acde0fe79a6a6da5cf6c8fe
Felix Draxle <felix.draxler@iwr.uni-heidelberg.de>
2023-03-08 14:37:39 +0100

    Optional validate_args for standard normal

--- FrEIA/distributions/normal.py
+++ FrEIA/distributions/normal.py
@@ -3,8 +3,12 @@ from torch.distributions import Independent, Normal
 
 
 class StandardNormalDistribution(Independent):
-    def __init__(self, *event_shape: int, device=None, dtype=None):
+    def __init__(self, *event_shape: int, device=None, dtype=None, validate_args=True):
         loc = torch.tensor(0., device=device, dtype=dtype).repeat(event_shape)
         scale = torch.tensor(1., device=device, dtype=dtype).repeat(event_shape)
 
-        super().__init__(Normal(loc, scale), len(event_shape))
+        super().__init__(
+            Normal(loc, scale, validate_args=validate_args),
+            len(event_shape),
+            validate_args=validate_args
+        )
